8eab75bf13ca00ad9128275b76a93941117afe60
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2020-09-09 17:48:24 -0300

    Initial Commit

--- JanKenPoUI/ViewController.swift
+++ JanKenPoUI/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var rock: UIButton!
+    @IBOutlet weak var paper: UIButton!
+    @IBOutlet weak var scissors: UIButton!
+    @IBOutlet weak var myChooseLabel: UILabel!
+    @IBOutlet weak var opponentChooseLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rock.setTitle("✊", for: .normal)
+        paper.setTitle("🖐", for: .normal)
+        scissors.setTitle("✌️", for: .normal)
+        
+        myChooseLabel.text = ""
+        opponentChooseLabel.text = ""
+        opponentChooseLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+        
+        
     }
 
 
+    @IBAction func handChosen(_ hand: UIButton) {
+        myChooseLabel.text = hand.currentTitle
+        opponentChooseLabel.text = getRandomHand()
+    }
+    
+    private func getRandomHand() -> String {
+        let hands = ["✊", "🖐", "✌️"]
+        
+        return hands.randomElement()!
+    }
 }
 
+//
+//extension Array {
+//    var randomElementPriquito: Element {
+//        let randomIndex = Int(arc4random_uniform(UInt32(self.count)))
+//        return self[randomIndex]
+//    }
+//}
