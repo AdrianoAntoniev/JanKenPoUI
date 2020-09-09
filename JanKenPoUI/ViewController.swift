c5379986404f1c851b60db5c4fa9a569ee5f3ac3
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2020-09-09 16:20:19 -0300

    Initial Commit

---
+++ JanKenPoUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JanKenPoUI
+//
+//  Created by Adriano Rodrigues Vieira on 09/09/20.
+//  Copyright © 2020 Adriano Rodrigues Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
