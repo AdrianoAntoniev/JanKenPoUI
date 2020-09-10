4315dd3f5359738ac24de934549459db6dc6c352
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2020-09-10 10:37:11 -0300

    Add game engine

---
+++ JanKenPoUI/Measures.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  JanKenPoUI
+//
+//  Created by Adriano Rodrigues Vieira on 09/09/20.
+//  Copyright © 2020 Adriano Rodrigues Vieira. All rights reserved.
+//
+
+import Foundation
+
+struct Measures {
+    static let NORMAL_SIZE_SCALE = 1.0
+    static let GROWN_SIZE_SCALE = 1.7
+    static let DURATION_OF_ANIMATION = 1.5
+}
+
+struct Texts {
+    static let DRAW = "Draw"
+    static let LOSE = "You lose!"
+    static let WIN = "You win!"
+}
+
+enum Hands: String {
+    case ROCK = "✊"
+    case PAPER = "🖐"
+    case SCISSORS = "✌️"
+}
+
--- JanKenPoUI/ViewController.swift
+++ JanKenPoUI/ViewController.swift
@@ -13,39 +13,97 @@ class ViewController: UIViewController {
     @IBOutlet weak var paper: UIButton!
     @IBOutlet weak var scissors: UIButton!
     @IBOutlet weak var myChooseLabel: UILabel!
-    @IBOutlet weak var opponentChooseLabel: UILabel!
+    @IBOutlet weak var opponentChooseLabel: UILabel! {
+        didSet {
+            opponentChooseLabel.transform  = opponentChooseLabel.transform.rotated(by: CGFloat.pi)
+        }
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        rock.setTitle("✊", for: .normal)
-        paper.setTitle("🖐", for: .normal)
-        scissors.setTitle("✌️", for: .normal)
+        rock.setTitle(Hands.ROCK.rawValue, for: .normal)
+        paper.setTitle(Hands.PAPER.rawValue, for: .normal)
+        scissors.setTitle(Hands.SCISSORS.rawValue, for: .normal)
         
         myChooseLabel.text = ""
         opponentChooseLabel.text = ""
-        opponentChooseLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         
-        
+        resultLabel.text = ""
     }
 
 
     @IBAction func handChosen(_ hand: UIButton) {
+        resultLabel.text = ""
+        
         myChooseLabel.text = hand.currentTitle
         opponentChooseLabel.text = getRandomHand()
+        
+        performAnimation(in: myChooseLabel)
+        performAnimation(in: opponentChooseLabel)
+        
+        if let myHand = myChooseLabel.text, let opponentHand = opponentChooseLabel.text {
+            defineWinner(between: myHand, and: opponentHand)
+        }
+        
+    }
+    
+    private func defineWinner(between player: String, and opponent: String) {        
+        if player == opponent {
+            resultLabel.text = Texts.DRAW
+        } else {
+            switch player {
+            case Hands.PAPER.rawValue:
+                if opponent == Hands.SCISSORS.rawValue {
+                    resultLabel.text = Texts.LOSE
+                } else {
+                    resultLabel.text = Texts.WIN
+                }
+            case Hands.SCISSORS.rawValue:
+                if opponent == Hands.PAPER.rawValue {
+                    resultLabel.text = Texts.WIN
+                } else {
+                    resultLabel.text = Texts.LOSE
+                }
+            case Hands.ROCK.rawValue:
+                if opponent == Hands.PAPER.rawValue {
+                    resultLabel.text = Texts.LOSE
+                } else {
+                        resultLabel.text = Texts.WIN
+                }
+            default:
+                break
+            }
+        }
+        
     }
     
     private func getRandomHand() -> String {
-        let hands = ["✊", "🖐", "✌️"]
+        let hands = [Hands.ROCK.rawValue, Hands.PAPER.rawValue, Hands.SCISSORS.rawValue]
         
         return hands.randomElement()!
     }
+    
+    private func performAnimation(in label: UILabel) {
+        UIViewPropertyAnimator.runningPropertyAnimator(
+            withDuration: Measures.DURATION_OF_ANIMATION,
+            delay: 0,
+            options: [],
+            animations: {
+                label.transform = CGAffineTransform.identity.scaledBy(x: CGFloat(Measures.GROWN_SIZE_SCALE), y: CGFloat(Measures.GROWN_SIZE_SCALE))
+            },
+            completion: { position in
+                UIViewPropertyAnimator.runningPropertyAnimator(
+                    withDuration: Measures.DURATION_OF_ANIMATION,
+                    delay: 0,
+                    options: [],
+                    animations: {
+                        label.transform = CGAffineTransform.identity.scaledBy(x: CGFloat(Measures.NORMAL_SIZE_SCALE), y: CGFloat(Measures.NORMAL_SIZE_SCALE))
+                    }
+                )
+            }
+        )
+    }
 }
-
-//
-//extension Array {
-//    var randomElementPriquito: Element {
-//        let randomIndex = Int(arc4random_uniform(UInt32(self.count)))
-//        return self[randomIndex]
-//    }
-//}
